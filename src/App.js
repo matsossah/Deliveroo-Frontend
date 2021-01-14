24cbb26cd5c034461068a8cac6e3ec0a2d9e5bea
Matéo Sossah <msossah@fb.com>
2021-01-15 00:10:53 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [infos, setInfos] = useState({});
+  const fetchData = async () => {
+    try {
+      // const response = await axios.get(
+      //   "https://deliveroo-backend28.herokuapp.com/"
+      // );
+      const response = await axios.get("http://localhost:3100/");
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>HELLO</h1>
     </div>
   );
 }
