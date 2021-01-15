fee10cf1cf935366e6d712e170fa7b297f4206e5
Matéo Sossah <msossah@fb.com>
2021-01-15 17:08:49 +0100

    Loading added

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import "./App.css";
 
 function App() {
   const [infos, setInfos] = useState({});
+  const [isLoading, setLoading] = useState(true);
+
   const fetchData = async () => {
     try {
       // const response = await axios.get(
       //   "https://deliveroo-backend28.herokuapp.com/"
       // );
       const response = await axios.get("http://localhost:3100/");
-      console.log(response.data);
+      setInfos(response.data);
+      setLoading(false);
     } catch (error) {
       console.log(error);
     }
@@ -21,8 +24,8 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <h1>HELLO</h1>
+    <div className="container">
+      {isLoading ? <h1>Loading...</h1> : <h1>{infos.restaurant.name}</h1>}
     </div>
   );
 }
